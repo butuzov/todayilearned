6fdcf5ce70e0a4ed64b8fcc00df969aca20d0680
Oleg Butuzov <butuzov@users.noreply.github.com>
2024-01-11 16:08:02 +0200

    docs: `jq` & `curl` updated (#45)

---
+++ devops/linux/tools/curl/readme.md
@@ -0,0 +1,22 @@
+# `curl`
+
+```shell
+# silent
+curl -s http://localhost:9200/_aliases
+```
+
+
+## Online Tooling
+
+### Poliglot UI
+
+- https://curlconverter.com/
+- https://scrape-it.cloud/curl-to-go-converter
+- https://www.scrapingbee.com/curl-converter
+
+### Golang
+
+- https://mholt.github.io/curl-to-go/
+- vice versa https://github.com/sethgrid/gencurl
+- vice versa https://github.com/moul/http2curl
+
--- devops/linux/tools/jq/csv.sh
+++ devops/linux/tools/jq/csv.sh
@@ -2,13 +2,13 @@
 echo "1231";
 
 # Example 1:
-# jq --slurp --raw-input --raw-output \
-#    'split("\n") | .[1:] | map(split(",")) |
-#     map({
-#         "PassengerId": .[0],
-#         "Survived": "\(.[1])",
-#         "Pclass": .[2],
-#       })' \
-#   example_titanic.csv
+jq --slurp --raw-input --raw-output \
+   'split("\n") | .[1:] | map(split(",")) |
+    map({
+        "PassengerId": .[0],
+        "Survived": "\(.[1])",
+        "Pclass": .[2],
+      })' \
+  example_titanic.csv
 
 
